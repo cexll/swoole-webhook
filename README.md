40fe502a5633e90ccc31376489a97b44683075e5
cexll <evanxian9@gmail.com>
2021-07-02 12:48:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,93 @@
+# swoole-webHook
+
+
+
+Using WebHooks to automatically pull code
+
+## Support
+
+* [x] GitHub
+* [x] Gitee
+
+## 依赖
+
+* php >= 7.2
+* ext-swoole >= 4.5
+
+## 安装
+
+```shell
+composer create-project cexll/swoole-webhook
+```
+
+## 配置
+
+1. 修改配置文件`config.json`
+
+`server`对应的是`Swoole\Http\Server`的相关配置
+
+* `ip`：IP地址
+* `port`：端口
+* `mode`：启动模式 `SWOOLE_BASE/SWOOLE_PROCESS`
+* `settings`：Server的配置
+
+> 正式运行时需要启动守护进程
+
+```json
+"server": {
+    "ip": "0.0.0.0",
+    "port": 19501,
+    "mode": 1,
+    "setting": {
+        "worker_num": 1,
+        "task_worker_num": 1,
+        "task_enable_coroutine": true
+    }
+},
+```
+
+`sites`对应的是项目的仓库等信息
+
+分为`github`和`gitee`，`key`是仓库名称，支持多个仓库。
+
+* `secret`/`password`：密钥/密码；`github`使用`secret`，`gitee`的 WebHook 密码使用`password`，签名密钥使用`secret`
+* `ref`：分支
+* `hook_name`：事件名称；`github`为`push`，`gitee`为`push_hooks`
+* `cmds`：需要执行的脚本
+
+```json
+"sites": {
+  "github": {
+      "cexll/swoole-webhook": {
+        "secret": "password",
+        "ref": "refs/heads/master",
+        "hook_name": "push",
+        "cmds": [
+          "git -C /yourpath/git-deploy pull"
+        ]
+      }
+  },
+  "gitee": {
+      "cexll/swoole-webhook": {
+        "password": "password",
+        "ref": "refs/heads/master",
+        "hook_name": "push_hooks",
+        "cmds": [
+          "git -C /yourpath/git-deploy pull"
+        ]
+      }
+  }
+}
+```
+
+2. 填写WebHook
+
+URL：`http://ip:port/github` or `http://ip:port/gitee`
+
+Secret/PassWord：对应`config.json`中的`secret/password`
+
+## 启动
+
+```shell
+php run.php
+```
---
+++ run.php
@@ -0,0 +1,181 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of swoole-webhook.
+ *
+ * @link     https://github.com/cexll/swoole-webhook
+ * @document https://github.com/cexll/swoole-webhook
+ * @license  https://github.com/cexll/swoole-webhook/blob/master/LICENSE
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+
+class run
+{
+    /** @var swoole_http_request */
+    protected  $server;
+
+    /** @var int */
+    protected  $port;
+
+    /** @var array */
+    protected $config;
+
+
+    public function __construct()
+    {
+        $this->config = self::getConfig();
+        $this->server = new swoole_http_server($this->config['server']['ip'], $this->config['server']['port'], $this->config['server']['mode']);
+        if (isset($this->config['server']['setting'])) {
+            $this->server->set($this->config['server']['setting']);
+        }
+        $this->server->on('request', function (swoole_http_request $request, swoole_http_response $response) {
+            $this->onRequest($request, $response);
+        });
+
+        $this->server->on('WorkerStart', static function () {
+        });
+
+        $this->server->on('task', function ($serv, $task) {
+            \Swoole\Coroutine::create(function () use ($serv, $task) {
+                $this->onTask($serv, $task);
+            });
+        });
+
+        $this->output();
+        $this->server->start();
+    }
+
+    public function output(): void
+    {
+        echo "Run SwooleWebHook", \PHP_EOL;
+    }
+
+    public function onRequest(swoole_http_request $request, swoole_http_response $response): void
+    {
+        try {
+            $this->log($request);
+            if (empty($request->rawcontent()) || $request->rawcontent() === "") {
+                $response->end('Not Found!');
+                return;
+            }
+            switch ($request->server['request_uri']) {
+                case '/gitee':
+                    $this->server->task([
+                        'type' => 'Gitee',
+                        'data' => $request->rawcontent(),
+                        'header' => $request->header,
+                    ]);
+                    break;
+                case '/github':
+                    $this->server->task([
+                        'type' => 'Github',
+                        'data' => $request->rawcontent(),
+                        'header' => $request->header,
+                    ]);
+                    break;
+                default:
+                    $response->write('<p>Not Found!</p>');
+                    break;
+            }
+        } catch (\Throwable $ex) {
+            $response->write($ex->getMessage());
+        }
+        $response->end('swoole webhook!');
+    }
+
+
+    public function parseGitee($data): void
+    {
+        $data['data'] = json_decode($data['data'], true);
+        foreach ($this->config['sites']['gitee'] as $item) {
+            if (isset($item['name']) && $item['name'] !== $data['data']['project']['path_with_namespace']) {
+                continue;
+            }
+            if (isset($item['password']) && $item['password'] !== $data['data']['password']) {
+                continue;
+            }
+            if (isset($item['hook_name']) && $item['hook_name'] !== $data['data']['hook_name']) {
+                continue;
+            }
+            if (isset($item['ref']) && $item['ref'] !== $data['data']['ref']) {
+                continue;
+            }
+            if (!isset($item['cmds']) || !is_array($item['cmds'])) {
+                break;
+            }
+            foreach ($item['cmds'] as $cmd) {
+                \Swoole\Coroutine::exec($cmd);
+            }
+            echo 'gitee hook', \PHP_EOL;
+            return;
+        }
+        echo 'gitee no action', \PHP_EOL;
+    }
+
+    public function parseGithub($data): void
+    {
+        $rawData = $data['data'];
+        $data['data'] = json_decode($data['data'], true);
+        foreach ($this->config['sites']['github'] as $item) {
+            if (isset($item['name']) && $item['name'] !== $data['data']['repository']['full_name']) {
+                continue;
+            }
+            if (isset($item['password'])) {
+                [$algo, $hash] = explode('=', $data['header']['x-hub-signature']);
+                $myHash = hash_hmac($algo, $rawData, $item['password']);
+                if ($hash !== $myHash) {
+                    continue;
+                }
+            }
+            if (isset($item['hook_name']) && $item['hook_name'] !== $data['header']['x-github-event']) {
+                continue;
+            }
+            if (isset($item['ref']) && $item['ref'] !== $data['data']['ref']) {
+                continue;
+            }
+            if (!isset($item['cmds']) || !is_array($item['cmds'])) {
+                break;
+            }
+            foreach ($item['cmds'] as $cmd) {
+                \Swoole\Coroutine::exec($cmd);
+            }
+            echo 'github hook', \PHP_EOL;
+
+            return;
+        }
+        echo 'github no action', \PHP_EOL;
+    }
+
+    public function onTask($serv, $task): void
+    {
+        switch ($task->data['type']) {
+            case 'Gitee':
+                $this->parseGitee($task->data);
+                break;
+            case 'Github':
+                $this->parseGithub($task->data);
+                break;
+        }
+    }
+
+    public function log(swoole_http_request $request): void
+    {
+        @file_put_contents(__DIR__ . '/http.log', json_encode([
+                'method' => $request->getMethod(),
+                'host' => $request->header['host'],
+                'url' => $request->server['path_info'],
+                'ip' => $request->server['remote_addr'],
+                'data' => $request->rawcontent(),
+            ]) . PHP_EOL, FILE_APPEND);
+    }
+    public static function getConfig()
+    {
+        return json_decode(file_get_contents(__DIR__ . '/config.json'), true);
+    }
+}
+
+
+\Swoole\Runtime::enableCoroutine(SWOOLE_HOOK_ALL);
+(new run());
